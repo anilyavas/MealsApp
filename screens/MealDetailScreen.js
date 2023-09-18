33ad9616903c6b02b45882fac2e8bfbaefbc5bd2
Anıl <anil@Anl-MacBook-Air.local>
2023-09-18 16:56:56 +0300

    button updated

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,16 +1,10 @@
-import {
-  Text,
-  View,
-  Image,
-  StyleSheet,
-  ScrollView,
-  Button,
-} from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
 import { MealDetails } from "../components/MealDetails";
 import { Subtitle } from "../components/MealDetail/Subtitle";
 import { List } from "../components/MealDetail/List";
 import { useLayoutEffect } from "react";
+import { IconButton } from "../components/IconButton";
 
 export function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId;
@@ -23,7 +17,13 @@ export function MealDetailScreen({ route, navigation }) {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title="Tap me!" onPress={headerButtonPressHandler} />;
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={headerButtonPressHandler}
+          />
+        );
       },
     });
   }, [navigation, headerButtonPressHandler]);
